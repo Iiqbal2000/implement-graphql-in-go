a5988bbad8973bc87cfdc2f653baad36d20bae4b
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2022-03-05 00:09:41 +0700

    feat: adding comments

--- error.go
+++ error.go
@@ -1,14 +1,18 @@
 package mygopher
 
+// Error represents custom error
 type Error struct {
 	Code    string `json:"code"`
 	Message string `json:"message"`
 }
 
+// Error implements error interface
 func (e Error) Error() string {
 	return e.Message
 }
 
+// Extensions implements ResolveError interface
+// see: https://github.com/graph-gophers/graphql-go#custom-errors
 func (e Error) Extensions() map[string]interface{} {
 	return map[string]interface{}{
 		"code":    e.Code,
